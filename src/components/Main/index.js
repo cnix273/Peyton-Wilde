caf47ee969f56608283b5603dfe3878773b05e19
Cameron Nix <camerondeverenix@gmail.com>
2022-01-29 17:48:30 -0500

    added Routing to home and about

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
 import './style.css';
 
-class Main extends Component {
-  render() {
-      return (
-          <div className="body">
-              <Nav/>
-              <Home/>
-          </div>
-      );
-  }
-}
+import Home from "../Home";
+import About from "../About";
 
+class Main extends Component {
+    render() {
+        return (
+            <Routes>
+                <Route exact path='/' element={<Home/>} />
+                <Route exact path='/about' element={<About/>} />
+            </Routes>
+        );
+    }
+}
+  
 export default Main;
