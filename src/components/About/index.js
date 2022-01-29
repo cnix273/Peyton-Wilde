6c192650154eb569703b5d3c29febbf59167e5d4
Cameron Nix <camerondeverenix@gmail.com>
2022-01-29 17:49:58 -0500

    added basic contents to test routing

---
+++ src/components/About/index.js
@@ -0,0 +1,9 @@
+const About= () => {
+    return (
+        <section id="about">
+            <h3>this is the About</h3> 
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section id="home">
+            <h3>this is the home</h3> 
+        </section>
+    )
+}
+
+export default Home;
